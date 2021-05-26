73cfe59b7be123bdb0f74ee318287d3abc10ac3c
Gabor <gabor.sarkozi@bearing.hu>
2021-05-26 15:45:44 +0200

    bemutatkozas

--- PROJEKTEK/Bemutatkozas/Shade 07 HTML/js/index.js
+++ PROJEKTEK/Bemutatkozas/Shade 07 HTML/js/index.js
@@ -1,3 +1,102 @@
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+	var window_height = $window.height();
+	var window_top_position = $window.scrollTop();
+	var window_bottom_position = (window_top_position + window_height);
+
+	$.each($animation_elements, function() {
+		var $element = $(this);
+		var element_height = $element.outerHeight();
+		var element_top_position = $element.offset().top;
+		var element_bottom_position = (element_top_position + element_height);
+
+		//check to see if this current container is within viewport
+		if ((element_bottom_position >= window_top_position) &&
+			(element_top_position <= window_bottom_position)) {
+		  $element.addClass('in-view');
+		} else {
+		  $element.removeClass('in-view');
+		}
+	});
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll', check_if_in_view);
+
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme-2').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1000);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme-3').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+
 
 (function ($) {
     "use strict";
