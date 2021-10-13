751fa8d0a98e6cf49bbeefce8491e2baeb9401fc
Gabor <gabor.sarkozi@bearing.hu>
2021-10-13 15:28:18 +0200

    arveres

--- PROJEKTEK/TESZTELÉS/bubbles.js
+++ PROJEKTEK/TESZTELÉS/bubbles.js
@@ -16,11 +16,11 @@
 }); */
 
 
-$(document).ready(function () {
+/*   $(document).ready(function () {
     $(window).on('scroll', function () {
-        var windowHeight = $(window).height(),
-            gridTop = windowHeight * .3,
-            gridBottom = windowHeight * .8;
+        var windowHeightOnScroll = $(window).height(),
+            gridTop = windowHeightOnScroll * .3,
+            gridBottom = windowHeightOnScroll * .8;
         $('.buying-items').each(function () {
             var thisTop = $(this).offset().top - $(window).scrollTop();
             if (thisTop > gridTop && (thisTop + $(this).height()) < gridBottom) {
@@ -31,5 +31,31 @@ $(document).ready(function () {
         });
     });
     $(window).trigger('scroll');
-});
+}); */
 
+var isInViewport = function(elem) {
+    var distance = elem.getBoundingClientRect();
+    return (
+      distance.top >= 0 &&
+      distance.left >= 0 &&
+      distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  // read the link on how above code works
+  var findMe = document.querySelectorAll('.buying-items');
+  
+  window.addEventListener('scroll', function(event) {
+  // add event on scroll
+  findMe.forEach(element => {
+      //for each .thisisatest
+      if (isInViewport(element)) {
+        //if in Viewport
+        element.classList.add("pulse-orange-big");
+      }
+      else {
+        element.classList.remove("pulse-orange-big");
+      }
+  });
+  }, false);
